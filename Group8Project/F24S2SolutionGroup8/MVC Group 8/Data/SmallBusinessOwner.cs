f78ecccaaa01fa01a400e16b2e16fd31ee8f56af
braelynnesandreth <braelynnesandreth@gmail.com>
2024-11-13 16:10:26 -0500

    update

---
+++ Group8Project/F24S2SolutionGroup8/MVC Group 8/Data/SmallBusinessOwner.cs
@@ -0,0 +1,18 @@
+﻿namespace MvcGroup8.Data
+{
+    internal class SmallBusinessOwner : AppUser
+    {
+        private string v1;
+        private string v2;
+        private string v3;
+        private string v4;
+
+        public SmallBusinessOwner(string v1, string v2, string v3, string v4)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+        }
+    }
+}
