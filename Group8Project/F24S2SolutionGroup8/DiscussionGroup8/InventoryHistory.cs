7966e4447c90a05b1df759b9276002c359b49e36
Samantha olivio <sbo00003@mix.wvu.edu>
2024-12-06 18:18:55 -0500

    fix inventory history

--- Group8Project/F24S2SolutionGroup8/DiscussionGroup8/InventoryHistory.cs
+++ Group8Project/F24S2SolutionGroup8/DiscussionGroup8/InventoryHistory.cs
@@ -9,27 +9,30 @@ namespace LibraryGroup8
     public class InventoryHistory
     {
         public int InventoryHistoryId { get; set; }
-
         public DateTime Date { get; set; }
-
         public int QuantityChange { get; set; }
         public string Reason { get; set; }
-        public int ProductId {get; set;}
+        public int ProductId { get; set; }
         public Product Product { get; set; }
 
+        // Constructor
         public InventoryHistory() { }
-        public InventoryHistory( DateTime date, int QuantityChange, string Reason)
+
+        public InventoryHistory(DateTime date,
+                                int quantityChange,
+                                string reason,
+                                int productId)
         {
-  
             Date = date;
-            this.QuantityChange = QuantityChange;
-            this.Reason = Reason;
-            this.ProductId = ProductId;
+            QuantityChange = quantityChange;
+            Reason = reason;
+            ProductId = productId;
+        }
 
-
-            
-
-            
+        // Override ToString for better display
+        public override string ToString()
+        {
+            return $"InventoryHistoryId: {InventoryHistoryId}, Date: {Date.ToShortDateString()}, QuantityChange: {QuantityChange}, Reason: {Reason}, ProductId: {ProductId}";
         }
     }
 }
