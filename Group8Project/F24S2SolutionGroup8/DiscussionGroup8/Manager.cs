f9860e6c20e2d2c0498c1ed49aa6ef162ae5a435
Samantha olivio <sbo00003@mix.wvu.edu>
2024-12-06 16:15:33 -0500

    add methods into manager

--- Group8Project/F24S2SolutionGroup8/DiscussionGroup8/Manager.cs
+++ Group8Project/F24S2SolutionGroup8/DiscussionGroup8/Manager.cs
@@ -7,20 +7,34 @@ namespace LibraryGroup8
 {
     public class Manager : AppUser
     {
-        public ICollection<Staff> StaffMembers { get; set; } 
+        public ICollection<Staff> StaffMembers { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public ICollection<InventoryHistory> InventoryHistories { get; set; }
 
         public Manager()
         {
             StaffMembers = new List<Staff>();
+            Products = new HashSet<Product>();
+            InventoryHistories = new HashSet<InventoryHistory>();
         }
 
         public Manager(string firstname, string lastname, string email, string password)
            : base(firstname, lastname, email, password)
         {
             StaffMembers = new List<Staff>();
+            Products = new List<Product>();
+            InventoryHistories = new List<InventoryHistory>();
         }
-        public RestockOrder ReviewInventoryHistory() => throw new System.NotImplementedException();
-     
+        public IEnumerable<InventoryHistory> ReviewInventoryHistory(int productId) => InventoryHistories.Where(h => h.ProductId == productId).ToList();
+
+
+        public IEnumerable<RestockOrder> AnalyzeRestockingTrends(int productId, DateTime startDate, DateTime endDate)
+        {
+            return Products
+                .FirstOrDefault(p => p.ProductId == productId)?.RestockOrder
+                .Where(r => r.Date >= startDate && r.Date <= endDate)
+                .ToList();
+        }
     }
 
     public class RestockingTrend
